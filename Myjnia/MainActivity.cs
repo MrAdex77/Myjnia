28cb56dcfd88e90eec5127cce316be718f4a43ab
Adrian Chmielewski <mradex77@gmail.com>
2020-07-03 11:23:53 +0200

    Layout of login

--- Myjnia/MainActivity.cs
+++ Myjnia/MainActivity.cs
@@ -13,7 +13,7 @@ namespace Myjnia
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.login);
         }
     }
 }
